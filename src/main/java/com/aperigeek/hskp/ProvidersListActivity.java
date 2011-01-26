e5fab6b14f078a2ef915a239c9b82206d2081820
Vivien Barousse <barousse.vivien@gmail.com>
2011-01-26 23:13:24 +0000

    Improved global application look and feel.

--- src/main/java/com/aperigeek/hskp/ProvidersListActivity.java
+++ src/main/java/com/aperigeek/hskp/ProvidersListActivity.java
@@ -15,13 +15,20 @@
 package com.aperigeek.hskp;
 
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.ResolveInfo;
+import android.database.DataSetObserver;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import java.util.List;
 
 /**
@@ -32,6 +39,73 @@ public class ProvidersListActivity extends ListActivity {
 
     private List<ResolveInfo> infos;
 
+    private class ProviderListAdapter implements ListAdapter {
+
+        private LayoutInflater inflater;
+
+        public ProviderListAdapter() {
+            inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        public boolean areAllItemsEnabled() {
+            return true;
+        }
+
+        public boolean isEnabled(int i) {
+            return true;
+        }
+
+        public void registerDataSetObserver(DataSetObserver dso) {
+            // Data set is fixed for now. Observers aren't required in this case
+        }
+
+        public void unregisterDataSetObserver(DataSetObserver dso) {
+            // Data set is fixed for now. Observers aren't required in this case
+        }
+
+        public int getCount() {
+            return infos.size();
+        }
+
+        public Object getItem(int i) {
+            return infos.get(i);
+        }
+
+        public long getItemId(int i) {
+            return i;
+        }
+
+        public boolean hasStableIds() {
+            return true;
+        }
+
+        public View getView(int i, View oldView, ViewGroup parent) {
+            // TODO: Reuse old view
+            LinearLayout element = (LinearLayout) inflater.inflate(R.layout.provider_list_item, parent, false);
+
+            TextView providerName = (TextView) element.findViewById(R.id.provider_name);
+            ImageView providerIcon = (ImageView) element.findViewById(R.id.provider_icon);
+
+            providerName.setText(infos.get(i).loadLabel(getPackageManager()));
+            providerIcon.setImageDrawable(infos.get(i).loadIcon(getPackageManager()));
+
+            return element;
+        }
+
+        public int getItemViewType(int i) {
+            return 0;
+        }
+
+        public int getViewTypeCount() {
+            return 1;
+        }
+
+        public boolean isEmpty() {
+            return infos.isEmpty();
+        }
+
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,14 +114,7 @@ public class ProvidersListActivity extends ListActivity {
         intent.addCategory("com.aperigeek.hskp.PROVIDER");
         infos = getPackageManager().queryIntentActivities(intent, 0);
 
-        Object[] results = new Object[infos.size()];
-        for (int i = 0; i < infos.size(); i++) {
-            results[i] = infos.get(i).loadLabel(getPackageManager()).toString();
-        }
-
-        setListAdapter(new ArrayAdapter(this,
-                R.layout.list_item,
-                results));
+        setListAdapter(new ProviderListAdapter());
     }
 
     @Override
