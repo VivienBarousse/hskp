79bffbf82f9c6ee065977d7dbe724190529741b2
Vivien Barousse <barousse.vivien@gmail.com>
2011-01-26 16:54:46 +0000

    Added license headers and @author tags.

--- src/main/java/com/aperigeek/hskp/provider/SampleProvider.java
+++ src/main/java/com/aperigeek/hskp/provider/SampleProvider.java
@@ -1,6 +1,15 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 package com.aperigeek.hskp.provider;
@@ -14,7 +23,7 @@ import com.aperigeek.hskp.R;
  * This class is a sample HotSpot provider, added for testing purposes. It has
  * no preferences, and doesn't do anything concrete.
  *
- * @author viv
+ * @author Vivien Barousse
  */
 public class SampleProvider extends ListActivity {
 
