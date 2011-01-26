ad62bc0c94757b00ed268f8e47641197fcd3bd81
Vivien Barousse <barousse.vivien@gmail.com>
2011-01-26 01:04:35 +0000

    Added first menu item to create new HotSpots.

--- src/main/java/com/aperigeek/hskp/MainActivity.java
+++ src/main/java/com/aperigeek/hskp/MainActivity.java
@@ -3,6 +3,8 @@ package com.aperigeek.hskp;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.ArrayAdapter;
 
 public class MainActivity extends ListActivity {
@@ -14,6 +16,13 @@ public class MainActivity extends ListActivity {
         setListAdapter(new ArrayAdapter(this,
                 R.layout.list_item,
                 new Object[] {"Item 1", "Item 2", "Item 3"}));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.list_menu, menu);
+        return true;
     }
 
 }
