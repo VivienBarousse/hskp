da6e0ae09f7bf2e3848b8f80efe588a35496f86d
Vivien Barousse <barousse.vivien@gmail.com>
2011-01-27 00:04:44 +0000

    Created sample provider project.
    This project will be used as a skeleton for new providers.

---
+++ hskp-providers/hskp-provider-sample/src/main/java/com/aperigeek/hskp/provider/sample/SampleProvider.java
@@ -0,0 +1,71 @@
+/*
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.aperigeek.hskp.provider.sample;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * This class is a sample HotSpot provider.
+ *
+ * It stores a simple username and password as properties.
+ *
+ * @author Vivien Barousse
+ */
+public class SampleProvider extends Activity implements OnClickListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.sample_provider);
+        
+        EditText username = (EditText) findViewById(R.id.username);
+        EditText password = (EditText) findViewById(R.id.password);
+
+        SharedPreferences prefs = getSharedPreferences("SampleProvider", 0);
+        username.setText(prefs.getString("username", null));
+        password.setText(prefs.getString("password", null));
+
+        Button buttonDone = (Button) findViewById(R.id.button_done);
+        Button buttonCancel = (Button) findViewById(R.id.button_cancel);
+        buttonDone.setOnClickListener(this);
+        buttonCancel.setOnClickListener(this);
+    }
+
+    public void onClick(View view) {
+        if (view.getId() == R.id.button_done) {
+            EditText username = (EditText) findViewById(R.id.username);
+            EditText password = (EditText) findViewById(R.id.password);
+
+            SharedPreferences prefs = getSharedPreferences("SampleProvider", 0);
+            Editor editor = prefs.edit();
+
+            editor.putString("username", username.getText().toString());
+            editor.putString("password", password.getText().toString());
+
+            editor.commit();
+        }
+        
+        finish();
+    }
+
+}
