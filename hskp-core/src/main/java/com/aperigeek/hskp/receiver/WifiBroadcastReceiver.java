e73b7a44e558aff2ac16a9c56d70d81eaf90a943
Colin Hebert <hebert.colin@gmail.com>
2011-01-27 00:44:11 +0000

    WiFi state change detection

---
+++ hskp-core/src/main/java/com/aperigeek/hskp/receiver/WifiBroadcastReceiver.java
@@ -0,0 +1,17 @@
+package com.aperigeek.hskp.receiver;
+
+import java.util.logging.Logger;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * @author Colin Hebert
+ */
+public class WifiBroadcastReceiver extends BroadcastReceiver{
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Logger.getLogger(WifiBroadcastReceiver.class.getCanonicalName()).info("WiFi state change detected");
+	}
+}
---
+++ hskp-core/src/main/java/com/aperigeek/hskp/service/HskpService.java
@@ -0,0 +1,15 @@
+package com.aperigeek.hskp.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * @author Colin Hebert
+ */
+public class HskpService extends Service {
+	@Override
+	public IBinder onBind(Intent intent) {
+		return null;
+	}
+}
