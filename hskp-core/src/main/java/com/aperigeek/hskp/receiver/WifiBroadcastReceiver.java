88f1ec8a85b03b121618bfaf69ff18abc62a9574
Vivien Barousse <barousse.vivien@gmail.com>
2011-03-03 19:53:42 +0000

    Implemented wifi event rebroadcasting.

--- hskp-core/src/main/java/com/aperigeek/hskp/receiver/WifiBroadcastReceiver.java
+++ hskp-core/src/main/java/com/aperigeek/hskp/receiver/WifiBroadcastReceiver.java
@@ -1,20 +1,27 @@
 
 package com.aperigeek.hskp.receiver;
 
-import java.util.logging.Logger;
-
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 
 /**
  * @author Colin Hebert
+ * @author Vivien Barousse
  */
 public class WifiBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Logger.getLogger(WifiBroadcastReceiver.class.getCanonicalName()).info("WiFi state change detected");
+        WifiManager manager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        WifiInfo connectionInfo = manager.getConnectionInfo();
+        String ssid = connectionInfo.getSSID();
+        if (ssid != null) {
+            Intent broadcastIntent = new Intent("com.aperigeek.hskp.receiver." + ssid);
+            context.sendBroadcast(broadcastIntent);
+        }
     }
 
 }
